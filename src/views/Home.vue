bbed423c544e7930d5a4022685d5a849fb77b131
Arunscape <8227297+Arunscape@users.noreply.github.com>
2019-02-17 16:12:06 -0700

    using some built in css classes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="home">
     <h1>This is a home page</h1>
+    <p
+      class="red white--text"
+    >Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo dicta deleniti voluptate enim architecto fuga ut molestiae ipsum vitae? Quaerat illum, consectetur maiores distinctio ab nisi incidunt pariatur dolores quia.</p>
+    <p
+      class="pink lighten-4 red--text text--darken-4"
+    >Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed repellendus commodi quae dolore nobis quisquam consectetur corrupti eveniet, impedit sit quod atque distinctio repudiandae sequi libero incidunt iste eum aliquid!</p>
+    <h1 class="display-4">Massive Display</h1>
+    <h1 class="display-1">Smol Text</h1>
+    <p class="headline">this is a headline</p>
+    <p class="subheading font-weight-bold">this is a subheading</p>
+    <p class="caption text-uppercase">this is a caption</p>
   </div>
 </template>
 
