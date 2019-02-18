54d71bd3ab0700f3335f4c4415c079fe536c5ef6
Arunscape <8227297+Arunscape@users.noreply.github.com>
2019-02-18 11:13:52 -0700

    simple toolbar

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,6 @@
 <template>
   <div class="home">
     <h1>Homepage</h1>
-
-    <v-btn class="hidden-md-and-down">Hidden on medium and smaller</v-btn>
-    <v-btn class="hidden-md-and-up">Hidden on medium and bigger</v-btn>
-    <v-btn class="hidden-sm-only">Hidden on small only</v-btn>
   </div>
 </template>
 
