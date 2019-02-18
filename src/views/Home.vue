3ad80ab51ae6bd107b9f47b8b1e49cdb4fede708
Arunscape <8227297+Arunscape@users.noreply.github.com>
2019-02-17 17:06:08 -0700

    stuff with buttons

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,28 @@
 <template>
   <div class="home">
-    <h1>This is a home page</h1>
-    <p
-      class="red white--text"
-    >Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo dicta deleniti voluptate enim architecto fuga ut molestiae ipsum vitae? Quaerat illum, consectetur maiores distinctio ab nisi incidunt pariatur dolores quia.</p>
-    <p
-      class="pink lighten-4 red--text text--darken-4"
-    >Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed repellendus commodi quae dolore nobis quisquam consectetur corrupti eveniet, impedit sit quod atque distinctio repudiandae sequi libero incidunt iste eum aliquid!</p>
-    <h1 class="display-4">Massive Display</h1>
-    <h1 class="display-1">Smol Text</h1>
-    <p class="headline">this is a headline</p>
-    <p class="subheading font-weight-bold">this is a subheading</p>
-    <p class="caption text-uppercase">this is a caption</p>
+    <v-btn class="pink white--text">This is a button</v-btn>
+    <v-btn depressed color="pink">hello</v-btn>
+    <v-btn flat color="pink">click me</v-btn>
+    <v-btn depressed class="pink white--text">
+      <v-icon left>cake</v-icon>The cake is a lie
+    </v-btn>
+
+    <v-btn depressed small class="pink white--text">
+      <v-icon small left>cake</v-icon>The cake is a lie
+    </v-btn>
+
+    <v-btn depressed large class="pink white--text">The cake is a lie
+      <v-icon large right>cake</v-icon>
+    </v-btn>
+
+    <v-btn depressed small dark color="purple">
+      <span>The cake is a lie</span>
+      <v-icon small right>cake</v-icon>
+    </v-btn>
+
+    <v-btn depressed small fab dark color="purple">
+      <v-icon small>cake</v-icon>
+    </v-btn>
   </div>
 </template>
 
