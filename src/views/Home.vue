d3266dde08d8f80942e94077d1260fb5e91e921b
Arunscape <8227297+Arunscape@users.noreply.github.com>
2019-02-18 11:03:40 -0700

    size breakpoints

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,28 +1,10 @@
 <template>
   <div class="home">
-    <v-btn class="pink white--text">This is a button</v-btn>
-    <v-btn depressed color="pink">hello</v-btn>
-    <v-btn flat color="pink">click me</v-btn>
-    <v-btn depressed class="pink white--text">
-      <v-icon left>cake</v-icon>The cake is a lie
-    </v-btn>
+    <h1>Homepage</h1>
 
-    <v-btn depressed small class="pink white--text">
-      <v-icon small left>cake</v-icon>The cake is a lie
-    </v-btn>
-
-    <v-btn depressed large class="pink white--text">The cake is a lie
-      <v-icon large right>cake</v-icon>
-    </v-btn>
-
-    <v-btn depressed small dark color="purple">
-      <span>The cake is a lie</span>
-      <v-icon small right>cake</v-icon>
-    </v-btn>
-
-    <v-btn depressed small fab dark color="purple">
-      <v-icon small>cake</v-icon>
-    </v-btn>
+    <v-btn class="hidden-md-and-down">Hidden on medium and smaller</v-btn>
+    <v-btn class="hidden-md-and-up">Hidden on medium and bigger</v-btn>
+    <v-btn class="hidden-sm-only">Hidden on small only</v-btn>
   </div>
 </template>
 
