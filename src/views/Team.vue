f83c684d56565b6d0d81d080661f5e13fe51ca7a
Arunscape <8227297+Arunscape@users.noreply.github.com>
2019-02-24 21:03:25 -0700

    deploy to gh pages

--- src/views/Team.vue
+++ src/views/Team.vue
@@ -36,23 +36,27 @@ export default {
         {
           name: "The Net Ninja",
           role: "Web developer",
-          avatar: "/avatar-1.png"
+          avatar: "avatar-1.png"
         },
         {
           name: "Ryu",
           role: "Graphic designer",
-          avatar: "/avatar-2.png"
+          avatar: "avatar-2.png"
         },
-        { name: "Chun Li", role: "Web developer", avatar: "/avatar-3.png" },
+        {
+          name: "Chun Li",
+          role: "Web developer",
+          avatar: "avatar-3.png"
+        },
         {
           name: "Gouken",
           role: "Social media maverick",
-          avatar: "/avatar-4.png"
+          avatar: "avatar-4.png"
         },
         {
           name: "Yoshi",
           role: "Sales guru",
-          avatar: "/avatar-5.png"
+          avatar: "avatar-5.png"
         }
       ]
     };
