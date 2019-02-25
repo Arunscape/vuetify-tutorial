5f8d1e449e5cc7dd9cea73194f4f8df4a051e057
Arunscape <8227297+Arunscape@users.noreply.github.com>
2019-02-24 17:01:52 -0700

    using a datepicker component

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -9,6 +9,19 @@
         <v-form class="px-3">
           <v-text-field label="Title" v-model="title" prepend-icon="folder"></v-text-field>
           <v-textarea label="Information" v-model="info" prepend-icon="edit"></v-textarea>
+
+          <v-menu>
+            <v-text-field
+              :value="formattedDate"
+              slot="activator"
+              label="Due date"
+              prepend-icon="date_range"
+            ></v-text-field>
+            <v-date-picker v-model="due"></v-date-picker>
+          </v-menu>
+
+          <v-spacer/>
+
           <v-btn flat class="success mx-0 mt-3" @click="submit">Add Project</v-btn>
         </v-form>
       </v-card-text>
@@ -17,16 +30,24 @@
 </template>
 
 <script>
+import format from "date-fns/format";
+
 export default {
   data() {
     return {
       title: "",
-      info: ""
+      info: "",
+      due: null
     };
   },
   methods: {
     submit() {
       console.log(this.title, this.info);
+    }
+  },
+  computed: {
+    formattedDate() {
+      return this.due ? format(this.due, "Do MMM YYYY") : "";
     }
   }
 };
